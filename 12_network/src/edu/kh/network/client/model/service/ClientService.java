54260e52227be72d7594cef15c74f30fc811ca6c
use-da <110652066+use-da@users.noreply.github.com>
2022-08-26 17:03:24 +0900

    0826

--- 12_network/src/edu/kh/network/client/model/service/ClientService.java
+++ 12_network/src/edu/kh/network/client/model/service/ClientService.java
@@ -1,9 +1,78 @@
 package edu.kh.network.client.model.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientService {
 
 	public void clientStart() {
 		
+		//1. 서버의 IP주소와 서버가 정한 포트번호를 매개변수로 하여 클라이언트용 소켓 객체 생성
+		
+		String serverIP="127.0.0.1";  //loop back IP(현재 컴퓨터를 나타내는 IP)
+		int port=8500;
+		
+		Socket clientSocket=null;
+		
+		InputStream is= null;
+		OutputStream os=null; 
+		
+		BufferedReader br=null;
+		PrintWriter pw=null;
+		
+		try {
+			System.out.println("[client]");
+			clientSocket=new Socket(serverIP, port);
+			//UnknownHostException : IP가 잘못 되었을 때 발생하는 예외
+			
+			//2. 서버와의 입출력 스트림 오픈
+			if(clientSocket!=null) { //서버와 연결이 성공한 경우
+				is=clientSocket.getInputStream();
+				os=clientSocket.getOutputStream();
+			//3. 보조 스트림을 통해 성능 개선	
+			br=new BufferedReader(new InputStreamReader(is));
+			pw=new PrintWriter(os);
+			//4. 스트림을 통해 읽고 쓰기
+			String serverMessage=br.readLine();
+			//서버->클라이언트에게 보낸 메세지를 저장
+			
+			//서버로부터 받은 메세지를 화면에 출력 
+			System.out.println(serverMessage);
+			
+			//클라이언트->서버로 메세지 전송
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.print("서버로 전달할 메세지 : ");
+			String str=sc.nextLine();
+			
+			pw.println(str); 
+			pw.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			
+			//5. 통신 종료
+			try {
+				//스트림/소켓 닫기
+				if(br!=null)br.close();
+				if(pw!=null)pw.close();
+				//보조스트림을 닫으면 연결된 기반 스트림도 같이 닫힌다
+				
+				if(clientSocket!=null)clientSocket.close();
+				
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+
 		
 	}
 
--- 12_network/src/edu/kh/network/server/model/service/ServerService.java
+++ 12_network/src/edu/kh/network/server/model/service/ServerService.java
@@ -1,5 +1,16 @@
 package edu.kh.network.server.model.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ServerService {
 
 	//TCP Socket프로그래밍
@@ -15,8 +26,88 @@ public class ServerService {
 	//ServerSocket : 포트와 연결되어 외부 요청을 기다리는 객체
 	//				 클라이언트의 연결 요청이 들어오면 클라이언트의 소켓을 얻어와 연결한다
 	//				 하나의 포트에는 하나의 ServerSocket만 연결가능
+
 	public void serverStart() {
+
+		//1. 서버의 포트번호 정함
+		int port=8500;  //0~65535 사이에 지정 가능(단,1023번 이하는 이미 사용중인 경우가 많으니 지정x)
 		
+		ServerSocket serverSocket=null;
+		Socket clientSocket = null;
+		
+		InputStream is=null;
+		OutputStream os=null;
+		
+		BufferedReader br=null;
+		PrintWriter pw=null;
+		
+		
+		try {
+			//2. 서버용 소켓 객체 생성
+			serverSocket=new ServerSocket(port);
+			
+			//3. 클라이언트 쪽에서 접속 요청이 오길 기다림
+			//4. 접속 요청이 오면 요청 수락 후 해당 클라이언트에 대한 소켓 객체 생성
+			
+			System.out.println("[Server]");
+			System.out.println("클라이언트의 요청을 기다리고 있습니다.");
+			
+			clientSocket=serverSocket.accept();
+			//클라이언트의 요청이 오고 수락이 될 때까지 무한 대기
+			System.out.println("[클라이언트 접속 성공]");
+			
+			//5. 연결된 클라이언트와 입출력 스트림 생성(얻어오기)
+			//->클라이언트가 서버요청시 소켓을 전달(소켓에 입/출력 스트림이 담겨있다)
+			is=clientSocket.getInputStream();
+			os=clientSocket.getOutputStream();
+			
+			//6. 보조 스트림을 통해 성능 개선
+			br=new BufferedReader(new InputStreamReader(is)); 
+			//문자 기반 BufferedReader와 바이트 기반 InputStream을 연결할 수 있도록 InputStreamReader를 사용
+			
+			pw=new PrintWriter(os);
+			
+			//7. 스트림을 통해 읽고 쓰기
+			//서버->클라이언트 메세지 전송
+			//2022년 08월 26일 15:30:21[서버 접속 성공]
+			Date now=new Date(); //java.util.Date : 자바에서 시간을 나타내는 객체
+								 //new Date()로 객체 생성 시 현재 시간이 저장됨
+			//SimpleDateFormat : 날짜형식을 간단히 지정
+			SimpleDateFormat sdf= new SimpleDateFormat("yyyy년MM월dd일 HH:mm:ss");
+			
+			String str=sdf.format(now)+"[서버 접속 성공]";
+			
+			pw.println(str); //서버->클라이언트로 출력
+			pw.flush();		 //flush() : 스트림의 내용을 밀어냄
+			
+			//클라이언트->서버 메세지 전달 받기
+			String clientMessage=br.readLine();
+			String clientIP=clientSocket.getInetAddress().getHostAddress();
+			
+			System.out.println(clientIP+"가 보낸 메세지 : "+ clientMessage);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			
+			//8. 통신 종료
+			try {
+				//스트림/소켓 닫기
+				if(br!=null)br.close();
+				if(pw!=null)pw.close();
+				//보조스트림을 닫으면 연결된 기반 스트림도 같이 닫힌다
+				
+				if(serverSocket!=null)serverSocket.close();
+				if(clientSocket!=null)clientSocket.close();
+				
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+			
+		}
+		
+
 		
 		
 		
